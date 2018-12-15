ac1c9456bbf3478b9e7314dc30e8132dc27005e1
Daniel <wendaniel@hotmail.com>
2018-12-14 23:48:32 -0500

    refactor to maven multi-modules. closes #1.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -10,7 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SfgPetClinicApplicationTests {
 
     @Test
-    public void contextLoads() { }
+    public void contextLoads() {
+
+    }
 
 }
 

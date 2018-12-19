221a142372cdd8ce0ff615dad4bd94bb0e67971b
Daniel <wendaniel@hotmail.com>
2018-12-18 20:52:29 -0500

    Added Contact Info Properties to Owners close #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,6 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephpne;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephpne() {
+        return telephpne;
+    }
+
+    public void setTelephpne(String telephpne) {
+        this.telephpne = telephpne;
+    }
+
     private Set<Pet> pets;
 
     public Set<Pet> getPets() {
